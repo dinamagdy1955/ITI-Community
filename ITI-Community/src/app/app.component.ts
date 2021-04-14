2d9a69ef98cc02bc622ce950e719541031e7dd02
dinamagdy1955 <dinamagdy.1995@gmail.com>
2021-04-14 22:40:19 +0200

    add user register and login and error components

--- ITI-Community/src/app/app.component.ts
+++ ITI-Community/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ITI-Community';
+  showHead: boolean = false;
+  constructor(private router: Router) {
+    // on route change to '/login', set the variable showHead to false
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if (event['url'] == '/Login' || event['url'] == '/Register/User') {
+          this.showHead = false;
+        } else {
+          this.showHead = true;
+        }
+      }
+    });
+  }
 }
