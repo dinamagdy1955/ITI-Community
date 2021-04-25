93e5d957cbae8b8f5e8e36583af1de4ae5d2921b
dinamagdy1955 <dinamagdy.1995@gmail.com>
2021-04-25 05:35:24 +0200

    changes

--- ITI-Community/src/app/Components/groups/Services/group-posts.service.ts
+++ ITI-Community/src/app/Components/groups/Services/group-posts.service.ts
@@ -1,42 +1,44 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { IPost } from '../ViewModel/ipost';
-import * as firestore from 'firebase/app'
+import * as firestore from 'firebase/app';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GroupPostsService {
-  likes: string[]
-  constructor(private db: AngularFirestore) { }
+  likes: string[];
+  constructor(private db: AngularFirestore) {}
 
   getGroupPost() {
-    return this.db.collection('GroupPosts').snapshotChanges()
+    return this.db.collection('GroupPosts').snapshotChanges();
   }
 
   giveLike(like, id) {
-
-    this.getGroupPost().subscribe((res) => {
-      this.likes = res.map(e => {
-        return {
-          ...(e.payload.doc.get("Likes"))
+    let sub = this.getGroupPost().subscribe((res) => {
+      res.map((e) => {
+        if (id == e.payload.doc.id) {
+          this.likes = e.payload.doc.get('Likes');
+          this.likes.push(like);
+          let LikeRef = this.db.collection('GroupPosts').doc(id);
+          LikeRef.ref.update({
+            Likes: this.likes,
+          });
+          sub.unsubscribe();
         }
-      })
-      console.log(this.likes)
-
-    })
-    console.log(like)
-    this.likes.push(like)
-    let LikeRef = this.db.collection('GroupPosts').doc(id);
-    LikeRef.ref.update({
-      Likes: this.likes
+      });
     });
   }
 
   writePost(post: IPost) {
     return new Promise<any>((res, rej) => {
-      this.db.collection('GroupPosts').add(post).then(res => { }, error => rej(error));
-    })
+      this.db
+        .collection('GroupPosts')
+        .add(post)
+        .then(
+          (res) => {},
+          (error) => rej(error)
+        );
+    });
   }
-
 }
--- ITI-Community/src/app/Components/groups/group-posts/group-posts.component.ts
+++ ITI-Community/src/app/Components/groups/group-posts/group-posts.component.ts
@@ -6,40 +6,43 @@ import { IPost } from '../ViewModel/ipost';
 @Component({
   selector: 'app-group-posts',
   templateUrl: './group-posts.component.html',
-  styleUrls: ['./group-posts.component.scss']
+  styleUrls: ['./group-posts.component.scss'],
 })
 export class GroupPostsComponent implements OnInit {
   postList;
   postGroupList = [];
   GroupId: string;
-  userID: string
-  constructor(private getall: GroupPostsService, private activeRoute: ActivatedRoute) {
-    this.userID = localStorage.getItem("uid")
+  userID: string;
+  constructor(
+    private getall: GroupPostsService,
+    private activeRoute: ActivatedRoute
+  ) {
+    this.userID = localStorage.getItem('uid');
   }
 
   ngOnInit(): void {
-    let param = this.activeRoute.paramMap.subscribe(res => {
-      this.GroupId = res.get('id')
-    })
-    this.getall.getGroupPost()
-      .subscribe(res => {
-        this.postList = res.map(e => {
-          return {
-            id: e.payload.doc.id,
-            ...(e.payload.doc.data() as object)
-          }
-        })
-        this.postGroupList = []
-        for (let post of this.postList) {
-          if (post.GroupId === this.GroupId) {
-            this.postGroupList.push(post)
-          }
+    let param = this.activeRoute.paramMap.subscribe((res) => {
+      this.GroupId = res.get('id');
+    });
+    this.getall.getGroupPost().subscribe((res) => {
+      // console.log('res', res);
+      this.postList = res.map((e) => {
+        // console.log('e.payload', e.payload.doc.data());
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data() as object),
+        };
+      });
+      this.postGroupList = [];
+      for (let post of this.postList) {
+        if (post.GroupId === this.GroupId) {
+          this.postGroupList.push(post);
         }
-      })
+      }
+    });
   }
 
   Like(like, id) {
-    this.getall.giveLike(like, id)
+    this.getall.giveLike(like, id);
   }
-
 }
