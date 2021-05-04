61c4b212eacedf249991e30811772b1642a35243
dinamagdy1955 <dinamagdy.1995@gmail.com>
2021-05-05 00:36:33 +0200

    edit header user card

--- ITI-Community/src/app/Components/profile/profile-body/profile-body-details/profile-body-details.component.ts
+++ ITI-Community/src/app/Components/profile/profile-body/profile-body-details/profile-body-details.component.ts
@@ -62,9 +62,7 @@ export class ProfileBodyDetailsComponent implements OnInit {
     const img = await this.us.uploadImg(selectedImg[0]);
     await img.ref.getDownloadURL().subscribe(async (url) => {
       this.us.editUserAvatar(this.uidLocal, url);
-      let userData = JSON.parse(localStorage.getItem('userData'));
-      userData.avatar = url;
-      localStorage.setItem('userData', JSON.stringify(userData));
+      localStorage.setItem('avatar', url);
       this.userDetails.avatar = url;
     });
   }
