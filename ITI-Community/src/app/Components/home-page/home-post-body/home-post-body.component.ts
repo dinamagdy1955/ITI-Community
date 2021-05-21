c9f619d099b911d8b3ff8712a3cbed42f8ddad13
dinamagdy1955 <dinamagdy.1995@gmail.com>
2021-05-21 23:53:14 +0200

    edit home posts

--- ITI-Community/src/app/Components/home-page/home-post-body/home-post-body.component.ts
+++ ITI-Community/src/app/Components/home-page/home-post-body/home-post-body.component.ts
@@ -4,72 +4,72 @@ import { HomePostsService } from '../HomeServices/home-posts.service';
 @Component({
   selector: 'app-home-post-body',
   templateUrl: './home-post-body.component.html',
-  styleUrls: ['./home-post-body.component.scss']
+  styleUrls: ['./home-post-body.component.scss'],
 })
 export class HomePostBodyComponent implements OnInit {
   MyPosts: any[] = [];
   frindsList: any[] = [];
-  MyFriendsPosts: any[]=[];
-  AllPosts:any[]=[]
+  MyFriendsPosts: any[] = [];
+  AllPosts: any[] = [];
   userID: string;
-  constructor(
-    private homePostServ:HomePostsService,
-   
-  ) {
+  flag = false;
+  constructor(private homePostServ: HomePostsService) {
     this.userID = localStorage.getItem('uid');
-   }
+  }
 
   ngOnInit(): void {
-
-    this.homePostServ.getAllMyPosts()
-    .subscribe((data) => {
-      this. AllPosts = data.map((e) => {
-         return {
-         id: e.payload.doc.id,
-         data: e.payload.doc.data()
-         };
-     
+    this.homePostServ.getAllMyPosts().subscribe((data) => {
+      data.map((e) => {
+        this.flag = false;
+        this.AllPosts.find((pos) => {
+          if (pos.id == e.payload.doc.id) {
+            this.flag = true;
+            pos.data = e.payload.doc.data();
+          }
+        });
+        if (!this.flag)
+          this.AllPosts.push({
+            id: e.payload.doc.id,
+            data: e.payload.doc.data(),
+          });
+        this.sort();
       });
-     // console.log(this.MyPosts);
     });
 
-    this.homePostServ.getAllMyFriendsPosts ()
-    .subscribe((data) => {
-      this.MyFriendsPosts = data.map((e) => {
-         return {
-         id: e.payload.doc.id,
-         data: e.payload.doc.data()
-         };
-     
+    this.homePostServ.getAllMyFriendsPosts().subscribe((data) => {
+      data.map((e) => {
+        this.flag = false;
+        this.AllPosts.find((pos) => {
+          if (pos.id == e.payload.doc.id) {
+            this.flag = true;
+            pos.data = e.payload.doc.data();
+          }
+        });
+        if (!this.flag)
+          this.AllPosts.push({
+            id: e.payload.doc.id,
+            data: e.payload.doc.data(),
+          });
+        this.sort();
       });
-      console.log(this.MyFriendsPosts);
-      this. AllPosts.push(...this.MyFriendsPosts)
-      console.log(this.AllPosts);
-      this.AllPosts = this.AllPosts.sort((a, b) => {
-        const date1 = a.data.PostedDate;
-        console.log(date1);
-        const date2 = b.data.PostedDate;
-        if (date1 > date2) {
-          return -1;
-        }
-        if (date1 < date2) {
-          return 1;
-        }
-        return 0;
-      });
-
     });
   }
 
-  
-
-  deletePost(pid,post) {
-    this.homePostServ.deletePost(pid,post)
+  sort() {
+    this.AllPosts = this.AllPosts.sort((a, b) => {
+      const date1 = a.data.PostedDate;
+      const date2 = b.data.PostedDate;
+      if (date1 > date2) {
+        return -1;
+      }
+      if (date1 < date2) {
+        return 1;
+      }
+      return 0;
+    });
   }
 
-
-
-
+  deletePost(pid, post) {
+    this.homePostServ.deletePost(pid, post);
   }
-
-
+}
