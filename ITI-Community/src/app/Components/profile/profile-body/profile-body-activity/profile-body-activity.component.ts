6b2db638c4f3dc3b72d222aedcc597bc424b7801
dinamagdy1955 <dinamagdy.1995@gmail.com>
2021-06-02 04:05:40 +0200

    edit profile

--- ITI-Community/src/app/Components/profile/profile-body/profile-body-activity/profile-body-activity.component.ts
+++ ITI-Community/src/app/Components/profile/profile-body/profile-body-activity/profile-body-activity.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { HomePostsService } from 'src/app/Components/home-page/HomeServices/home-posts.service';
+import { UserService } from 'src/app/MainServices/User.service';
 
 @Component({
   selector: 'app-profile-body-activity',
   templateUrl: './profile-body-activity.component.html',
-  styleUrls: ['./profile-body-activity.component.scss']
+  styleUrls: ['./profile-body-activity.component.scss'],
 })
 export class ProfileBodyActivityComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  subscriptions: Subscription[] = [];
+  userID;
+  data: Observable<any>;
+  homePosts = [];
+  constructor(
+    private us: UserService,
+    private homePostsServ: HomePostsService
+  ) {
+    this.data = this.us.localUserData.asObservable();
+    let sub = this.data.subscribe((res) => {
+      if (res != null) {
+        this.userID = res.id;
+      }
+    });
+    this.subscriptions.push(sub);
   }
 
+  ngOnInit() {
+    let sub = this.homePostsServ.getMyPosts(this.userID).subscribe((res) => {
+      this.homePosts = res.map((e) => {
+        return {
+          id: e.payload.doc.id,
+          data: e.payload.doc.data(),
+        };
+      });
+    });
+    this.subscriptions.push(sub);
+  }
 }
